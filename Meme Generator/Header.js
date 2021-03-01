34a53b13bc810747482f31e0ac5a42da56d5db76
Devansh <daa.devansh@gmail.com>
2021-03-01 22:53:46 +0530

    Adding Projects

---
+++ Meme Generator/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <img src="http://www.relatably.com/m/img/black-and-white-meme-background/500px-Meme_poker_face_white_background_1920x1200_wallpaper_www.wallpaperfo.com_73.jpg" 
+            alt="problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
